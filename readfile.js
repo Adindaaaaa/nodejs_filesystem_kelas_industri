f806e8c7fdb7050e9bf20e0b1bcb1bef3ccc655c
Adindaaaaa <adindansw1301@gmail.com>
2024-09-22 16:12:07 +0700

    biodata adinda

--- readfile.js
+++ readfile.js
@@ -1,5 +1,5 @@
 const fs = require('node:fs');
-fs.readFile('Hello.txt', 'utf8', function(error, data) {
+fs.readFile('Biodata.txt', 'utf8', function(error, data) {
     if(error) {console.error(error)
     }else{
         console.log('adin :', data)
--- writefile.js
+++ writefile.js
@@ -1,5 +1,5 @@
 const fs = require('node:fs');
-fs.writeFile('Hello.txt', 'Hello nama saya adinda',
+fs.writeFile('Biodata.txt', 'Hello nama saya adinda, kelas XI TJKT 2, Alamat saya di jl Kramat awirarangan setu, umur 16 tahun',
 function(error) {
     if (error){
         console.error ('Terjadi Error', error);
